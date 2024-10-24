100edfc1e99a24e30f877e38649982e3a6ee9682
Vinibiiel <vinibiiel123452@hotmail.com>
2024-10-24 19:06:58 -0300

    feature: troca de arquitetura para maven e adição de um pom.xml

---
+++ src/main/java/exception/PapelMoedaInvalidaException.java
@@ -0,0 +1,9 @@
+package main.java.exception;
+
+/**
+ *
+ * @author Calebe de Paula Bianchini
+ */
+public class PapelMoedaInvalidaException extends Exception {
+    
+}
---
+++ src/test/java/TestTicketMachine.java
@@ -0,0 +1,11 @@
+package test.java;
+
+import main.java.TicketMachine;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestTicketMachine {
+    
+}
