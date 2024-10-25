b0423eca963e4f61b1036a305060cd0a04c7bb59
Vinibiiel <vinibiiel123452@hotmail.com>
2024-10-24 22:25:55 -0300

    feat: add tests for troco

--- src/test/java/TestTroco.java
+++ src/test/java/TestTroco.java
@@ -1,11 +1,24 @@
 package test.java;
 
 import main.java.br.calebe.core.Troco;
+import main.java.br.calebe.core.PapelMoeda;
 
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Iterator;
 
 public class TestTroco {
-    
+
+    @Test
+    public void TesteCriacaoTroco() {
+        Troco troco = new Troco(5);
+        assertNotNull(troco, "A instancia deve ser criada com valor inteiro");
+    }
+
+    @Test
+    public void TesteCriacaoTrocoGetTrocoIterator() {
+        Troco troco = new Troco(5);
+        assertTrue(troco.getIterator() instanceof Iterator<PapelMoeda> , "O troco deve ser do tipo PapelMoeda");
+    }
+
 }
